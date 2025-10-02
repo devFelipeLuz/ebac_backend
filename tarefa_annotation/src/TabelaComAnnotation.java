2e73aa661c33db709f9f8409456970ce2752c10b
Felipe Luz <felipetluz@outlook.com>
2025-10-02 13:57:07 -0300

    default commit

---
+++ tarefa_annotation/src/TabelaComAnnotation.java
@@ -0,0 +1,8 @@
+
+@Tabela(nome = "TabelaComAnnotation")
+public class TabelaComAnnotation {
+
+    public static void main (String[] args) {
+        System.out.println("****** Tarefa Annotation ******");
+    }
+}
